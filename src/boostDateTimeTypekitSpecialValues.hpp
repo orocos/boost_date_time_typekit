2c2f2212cfaae2017c204cba6c207931bfcf4c1e
Luca Magnabosco <luca@intermodalics.eu>
2016-12-19 15:19:38 +0100

    fix catkinize

--- src/boostDateTimeTypekitSpecialValues.hpp
+++ src/boostDateTimeTypekitSpecialValues.hpp
@@ -36,7 +36,7 @@
 #ifndef ORO_BOOST_DATE_TIME_TYPEKIT_SPECIALVALUES_HPP
 #define ORO_BOOST_DATE_TIME_TYPEKIT_SPECIALVALUES_HPP
 
-#include "boostDateTimeTypekitTypes.hpp"
+#include <boost_date_time_typekit/typekit/Types.hpp>
 
 #include <ostream>
 #include <istream>
